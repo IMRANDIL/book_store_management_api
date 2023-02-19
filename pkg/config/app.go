2d9894c1d7962362a808cffccba9eb6a048050eb
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 10:38:51 +0530

    added the database config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -9,7 +9,7 @@ var (
 )
 
 func Connect () {
-	d, err := gorm.Open("mysql","root:Dil@2580123@tcp(localhost:3306)/book_store?")
+	d, err := gorm.Open("mysql","root:Dil@2580123@tcp(localhost:3306)/book_store?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
