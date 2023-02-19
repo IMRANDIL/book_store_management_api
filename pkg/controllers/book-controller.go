06918d8371c710b91decc977e9fdeebf52ddf921
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 15:07:39 +0530

    some more done

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,28 @@
 package controllers
+
+import (
+	"book_store_api/pkg/models"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+
+var NewBook models.Book
+
+
+func GetAllBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBook()
+
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+}
