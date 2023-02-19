5bf66e9dd2d5db1df228a8134c8d8ea661e6f907
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 15:14:04 +0530

    added the controller as well

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"book_store_api/pkg/models"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -25,4 +27,16 @@ func GetAllBook(w http.ResponseWriter, r *http.Request) {
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	bookId := params["bookId"]
+
+	ID, err := strconv.ParseInt(bookId,0,0)
+
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
