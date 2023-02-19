b011f06142528628fcdedc0cb9d2a2e9677f5d9b
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 10:50:25 +0530

    some more go code done

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,9 +1,17 @@
 package utils
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 
 
-func ParseBody (r *http.Request) {
-
+func ParseBody (r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return 
+		}
+	}
 }
