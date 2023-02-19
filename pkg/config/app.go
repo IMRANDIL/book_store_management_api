41460f0793a509f70e5eb3c3d97a1111906cb1d0
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 10:32:55 +0530

    some more done

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,2 +1,17 @@
 package config
 
+import "github.com/jinzhu/gorm"
+
+
+
+var (
+	db *gorm.DB
+)
+
+func Connect () {
+	d, err := gorm.Open("mysql","root:Dil@2580123@tcp(localhost:3306)/book_store?")
+
+	if err != nil {
+		panic(err)
+	}
+}
