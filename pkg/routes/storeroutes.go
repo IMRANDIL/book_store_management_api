3b95d49748336c0e27dc82276960fd6879459588
IMRANDIL <aliimranadil2@gmail.com>
2023-02-19 09:52:22 +0530

    started adding config

--- pkg/routes/storeroutes.go
+++ pkg/routes/storeroutes.go
@@ -1,8 +1,14 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+)
 
 
 var RegisterBookStoreRoutes = func (router *mux.Router)  {
-	
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetAllBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
