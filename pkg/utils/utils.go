a67be39f9cbd804348dfb4c28cd2a33dae81e9be
ALI IMRAN ADIL <aliimranadil2@gmail.com>
2023-03-05 15:49:36 +0530

    Update utils.go
    some improvements to this parse body method utils function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -8,10 +8,15 @@ import (
 
 
 
-func ParseBody (r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
-			return 
-		}
+func ParseBody(r *http.Request, x interface{}) error {
+	if r.Method != "POST" && r.Method != "PUT" {
+		return fmt.Errorf("Invalid request method: %s", r.Method)
 	}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(x); err != nil {
+		return err
+	}
+
+	return nil
 }
